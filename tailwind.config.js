95ed00f278fcc0a5e4e263843ec7c07956eb7698
Onyeka <oaribeana@gmail.com>
2021-11-15 21:15:49 +0100

    First commit for project

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    container: {
+      center: true,
+      padding: "4rem",
+    },
+
+    extend: {
+      colors: {
+        violet: "#674baf",
+        magenta: "#e882e8",
+      },
+      fontFamily: {
+        body: ["Poppins", "sans-serif"],
+        text: ["Open Sans", "sans-serif"],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
