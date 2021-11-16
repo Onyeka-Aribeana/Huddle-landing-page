5b9aebe4d0ec555ddd21e2cf3e084e9c22d82a5e
Onyeka <oaribeana@gmail.com>
2021-11-16 03:11:43 +0100

    Fix: image links

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,8 +17,8 @@ module.exports = {
         text: ["Open Sans", "sans-serif"],
       },
       backgroundImage: {
-        desktop: "url('./docs/images/bg-desktop.svg')",
-        mobile: "url('./docs/images/bg-mobile.svg')",
+        desktop: "url('./images/bg-desktop.svg')",
+        mobile: "url('./images/bg-mobile.svg')",
       },
     },
   },
